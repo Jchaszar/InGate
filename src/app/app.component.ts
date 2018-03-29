d482582581fefe1636397d9f653d438268142d1b
James M Chaszar <jchaszar@email.sc.edu>
2018-03-28 21:01:25 -0400

    linked firebase in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+import {firebaseConfig} from './firebaseCredentials';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -28,6 +30,7 @@ export class MyApp {
   }
 
   initializeApp() {
+    firebase.initializeApp(firebaseConfig);
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
---
+++ src/app/firebaseCredentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1rUVvJ5KxyBg0Ufs3ksEylrpkMsc7IlE",
+    authDomain: "ingate-584e9.firebaseapp.com",
+    databaseURL: "https://ingate-584e9.firebaseio.com",
+    projectId: "ingate-584e9",
+    storageBucket: "ingate-584e9.appspot.com",
+    messagingSenderId: "987519674154"
+  };
+  
