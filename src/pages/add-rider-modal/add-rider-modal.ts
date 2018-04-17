6bc7cdc536a26e0b0c609afc10865897f894f5fe
Daniel Berry <berryfd1995@gmail.com>
2018-04-17 19:17:16 -0400

    Redid add rider model, added search bar, fixed pushing and popping

--- src/pages/add-rider-modal/add-rider-modal.ts
+++ src/pages/add-rider-modal/add-rider-modal.ts
@@ -9,41 +9,65 @@ import firebase from 'firebase';
   templateUrl: 'add-rider-modal.html',
 })
 export class AddRiderModalPage {
+  staticRiders = [];
 	availableRiders = [];
 	classRiders = [];
 	riderRef;
 	selectedRider;
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   	this.riderRef = firebase.database().ref('Riders/');
-  	this.riderRef.on('value', (snap) => {
-  		snap.forEach((child) => {
-  			if(child.val().fullName == null){
-  				console.log('.');
-  			}
-  			else{
-  				let newRider = {
-  					fullName : child.val().fullName,
-  					id: child.val().key,
-  					email : child.val().email,
-  				}
-  				this.availableRiders.push(newRider);
-  			}
-  		})
-  	})
+     this.initilizeItems();
   }
   cancel(){
   	this.viewCtrl.dismiss();
  }
  save(){
  	console.log(this.classRiders);
- 	this.viewCtrl.dismiss(this.classRiders);
+  this.staticRiders.forEach((data) => {
+      if(data.selected){
+         this.classRiders.push(data);
+      }
+  });
+ this.viewCtrl.dismiss(this.classRiders);
  }
-
+ initilizeItems(){
+      this.availableRiders = [];
+      this.riderRef.on('value', (snap) => {
+         snap.forEach((child) => {
+        if(child.val().fullName == null){
+          console.log('.');
+        }
+        else{
+          let newRider = {
+            fullName : child.val().fullName,
+            id: child.val().key,
+            email : child.val().email,
+            selected: false
+          }
+          this.staticRiders.push(newRider);
+          this.availableRiders.push(newRider);
+        }
+      })
+    })
+ }
+ filterItems(ev: any){
+   this.availableRiders = this.staticRiders;
+   let val = ev.target.value;
+   if(val && val.trim() !== ''){
+     this.availableRiders = this.availableRiders.filter((item) => {
+        return (item.fullName.toLowerCase().includes(val.toLowerCase()));
+     });
+   }
+ }
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddRiderModalPage');
   }
   updateRiders(rider){
-  	this.classRiders.push(rider);
+  	this.staticRiders.forEach((data) => {
+        if(data.id == rider.id){
+          data.selected = true;
+        }
+    });
   	console.log(this.classRiders);
   }
 
