5d10bb03b0291de72fa57b449a6fea1f075723e6
Daniel Berry <berryfd1995@gmail.com>
2018-04-17 21:54:27 -0400

    Grabbed all the registered classes for riders. Added in style sheet alex used on Organizer Home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,14 +3,14 @@ import { NavController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import firebase from 'firebase';
 
-export class Clss {
-	public ID: string;
-	public ClassIDref: number;
-	public Title: string;
-	public Description: string;
-	public ESTStart: number;
-	public Index: number; 
-	public Riders: string[];
+export class Clazz {
+	public ClassID: number;
+	public ClassIDref: string;
+	public ClassTitle: string;
+	public ClassDescription: string;
+	public ClassESTStart: number;
+	public ClassIndex: number; 
+	public ClassRiders: Rider[] =  [];
 	//Division Info
 	public DivisionIDref: string;
 	public DivisionName: string;
@@ -37,7 +37,7 @@ export class Rider{
 
 export class HomePage {
 	private event = [];
-	public registeredClasses: Clss[] = [];
+	public registeredClasses: Clazz[] = [];
 	public currentRider: Rider = new Rider();
 	private eventRef;
 	private riderRef;
@@ -51,6 +51,12 @@ export class HomePage {
 		this.navCtrl.setRoot("LoginPage");
 	});
   }
+  viewClass(clazz: Clazz){
+
+  }
+
+
+
   ionViewWillEnter(){
   	this.riderRef.on('value', (data) => {
   		if(data.val() != null){
@@ -62,19 +68,81 @@ export class HomePage {
   		}
   	});  
   	console.log("called");	
-  	let data
+  	//Get the list of event ID's
   	this.eventRef.on('value', (events) => {
   		if(events.val() != null){
+  			//Iterate over event ID's
   		events.forEach((event) => {
   			if(event.val() != null){
-  				var id = event.key;
-  				var tempRef = firebase.database().ref('Events/' + id);
-  				tempRef.on('value', (schools) => {
-  					if(schools.val() != null){
-  						schools.forEach((school) => {
+  				var eventIDref = event.key;
+  				//Grab snapshot of an event ID
+  				var tempRef = firebase.database().ref('Events/' + eventIDref);
+  				//Event data can be accessed here on out
+  				//Grab each value (we are looking for the divisions/schools)
+  				tempRef.on('value', (eventData) => {
+  					if(eventData.val() != null){
+  						//Iterate over the values in the event
+  						eventData.forEach((school) => {
   							if(school.val() != null){
+  								//if the school does not have a child called division name, ignore. Not a school/division
   								if(school.val().divisionName != null){
-  									console.log(school.val().divisionName);
+  									var schoolIDref = school.key;
+  									//Grab snapshot of the school/division
+  									var schoolRef = firebase.database().ref('Events/' + eventIDref + '/' + schoolIDref);
+  									//We can now access division name/ring/startdate/starttime
+  									schoolRef.on('value', (schoolData) => {
+  										if(schoolData != null){
+  											//Iterate over each class
+  											schoolData.forEach((clazz) => {
+  												if(clazz.val() != null){
+  													//if the value does not have a child with a key called className, then it is not a class
+  													if(clazz.val().className != null){
+  														var clazzIDref = clazz.key;
+  														var clazzRef = firebase.database().ref('Events/' + eventIDref + '/' + schoolIDref + '/' + clazzIDref);
+  														//We can now access class Description/delay/name/idref
+  														clazzRef.on('value', (clazzData) => {
+  															if(clazzData != null){
+  																var clazzRiders = firebase.database().ref('Events/' + eventIDref + '/' + schoolIDref + '/' + clazzIDref + '/riders');
+  																clazzRiders.on('value', (riders) => {
+  																	if(riders != null){
+  																		riders.forEach((rider) => {
+  																			if(rider.val().id === this.currentRider.ID){
+  																				var clazz = new Clazz();
+  																				//Store Event Data in clazz
+  																				clazz.EventDate = eventData.val().date;
+  																				clazz.EventLocation = eventData.val().location;
+  																				clazz.EventName = eventData.val().name;
+  																				clazz.EventIDref = eventIDref;
+  																				//Store Division Data in clazz
+  																				clazz.DivisionIDref = schoolIDref;
+  																				clazz.DivisionName = schoolData.val().divisionName;
+  																				clazz.DivisionRing = schoolData.val().divisionRing;
+  																				clazz.DivisionStartDate = schoolData.val().divisionStartDate;
+  																				clazz.DivisionStartTime = schoolData.val().divisionStartTime;
+  																				//Store Class Data in clazz
+  																				clazz.ClassIDref = clazzIDref;
+  																				clazz.ClassID = clazzData.val().classRefID;
+  																				clazz.ClassTitle = clazzData.val().className;
+  																				clazz.ClassDescription = clazzData.val().classDescription;
+  																				clazz.ClassESTStart = clazzData.val().classDelay;
+  																				riders.forEach((r) => {
+  																					var newRider = new Rider();
+  																					newRider.ID = r.val().id;
+  																					newRider.Name = r.val().fullName;
+  																					clazz.ClassRiders.push(newRider);
+  																				});
+  																				this.registeredClasses.push(clazz);
+  																			}
+  																		});
+  																	}
+  																});
+  															}
+  														});
+  													}
+  												}
+  											});
+  										}
+  									});
   								}
   							}
   						});
